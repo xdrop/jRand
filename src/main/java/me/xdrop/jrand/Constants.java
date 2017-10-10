7fb287f9429d567642e03360a8be93be9718bb83
Adam <adam.galtrey@gmail.com>
2017-10-10 22:33:09 +0100

    Tidy up constants.

--- src/main/java/me/xdrop/jrand/Constants.java
+++ src/main/java/me/xdrop/jrand/Constants.java
@@ -1,27 +1,21 @@
 package me.xdrop.jrand;
 
 public class Constants {
+
     public static String alphaLowerPool = "abcdefghijklmnopqrstuvwxyz";
     public static String alphaUpperPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static String numericPool    = "1234567890";
     public static String symbolsPool    ="!@#$%^&*()[]";
 
-    public static char[] alphaLowerPoolChar = new char[] {
-            'a','b','c','d','e','f','g',
-            'h','i','j','k','l','m','n',
-            'o','p','q','r','s','t','u',
-            'v','w','x','y','z'};
+    /*
+        Probably don't need these, but provide a direct char[] interface.
+     */
+    public static char[] alphaLowerPoolChar = alphaLowerPool.toCharArray();
 
-    public static char[] alphaUpperPoolChar = new char[] {
-            'A','B','C','D','E','F','G',
-            'H','I','J','K','L','M','N',
-            'O','P','Q','R','S','T','U',
-            'V','W','X','Y','Z'};
+    public static char[] alphaUpperPoolChar = alphaUpperPool.toCharArray();
 
-    public static char[] numericPoolChar = new char[] {
-            '1','2','3','4','5','6','7','8','9','0'};
+    public static char[] numericPoolChar = numericPool.toCharArray();
 
-    public static char[] symbolsPoolChar = new char[] {
-            '!','@','#','$','%','^','&','*','(',')','[',']'
-    };
+    public static char[] symbolsPoolChar = symbolsPool.toCharArray();
+
 }
