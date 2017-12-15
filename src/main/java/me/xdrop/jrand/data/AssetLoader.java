f56a70e989899f6ab471fb023d380ccbada63db2
xdrop <xdrop.me@gmail.com>
2017-12-15 12:12:23 +0000

    Update asset loader

--- src/main/java/me/xdrop/jrand/data/AssetLoader.java
+++ src/main/java/me/xdrop/jrand/data/AssetLoader.java
@@ -1,15 +1,20 @@
 package me.xdrop.jrand.data;
 
 import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AssetLoader {
     public List<String> loadAsset(String assetName) {
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(assetName).getFile());
+        URL filename = classLoader.getResource(assetName);
+
+        if (filename == null) return Collections.emptyList();
+
+        File file = new File(filename.getFile());
         List<String> list = new ArrayList<>();
-        reverse(list);
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             while(reader.ready()) {
@@ -23,6 +28,7 @@ public class AssetLoader {
     }
 
     public static void reverse(List<?> list) { rev(list); }
+
     private static <T> void rev(List<T> list) {
         List<T> tmp = new ArrayList<T>(list);
         for (int i = 0; i < list.size(); i++) {
