2cfe715aba24fc8fbcccd8264d116d9d1ad08b23
xdrop <xdrop.me@gmail.com>
2018-01-16 18:23:56 +0000

    Update package.sh

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
 mvn versions:set -DnewVersion=$1
 version=$(cat VERSION)
-find -E . -iregex '.*(README|_coverpage).md' -exec sed -i .bak -e s/$version/$1/g {} \;
-sed -E -i .bak -e s/documentation-(.+)-green.svg/documentation-$(cat version | sed 's/-/--/g)-green.svg/g README.md
+find . -iregex '.*(README|_coverpage).md' -exec sed -i .bak -e s/$version/$1/g {} \;
+sed -E -i .bak -e "s/documentation-(.+)-green.svg/documentation-$(echo $version | sed 's/-/--/g')-green.svg/g" README.md
