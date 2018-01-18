1b66a1021573e38f806fd7879b8fe664dfa35cd9
xdrop <xdrop.me@gmail.com>
2018-01-18 18:46:55 +0000

    Update regex on versioning script

--- version.sh
+++ version.sh
@@ -2,5 +2,5 @@
 
 mvn versions:set -DnewVersion=$1
 version=$(cat VERSION)
-find . -iregex '.*(README|_coverpage).md' -exec sed -i .bak -e s/$version/$1/g {} \;
+find . -iregex '.*\(README\|_coverpage\).md' -exec sed -i .bak -e s/$version/$1/g {} \;
 sed -E -i .bak -e "s/documentation-(.+)-green.svg/documentation-$(echo $version | sed 's/-/--/g')-green.svg/g" README.md
