0c19dbaf6d91c8be3b68027a3bc6dd886aab22f1
xdrop <xdrop.me@gmail.com>
2018-01-31 20:18:26 +0000

    Add latitude generator

---
+++ jrand-core/src/main/java/me/xdrop/jrand/generators/location/LatitudeGenerator.java
@@ -0,0 +1,45 @@
+package me.xdrop.jrand.generators.location;
+
+import me.xdrop.jrand.Generator;
+import me.xdrop.jrand.annotation.Facade;
+import me.xdrop.jrand.generators.basics.DecimalGenerator;
+import me.xdrop.jrand.model.RangeOption;
+
+@Facade(accessor = "latitude")
+public class LatitudeGenerator extends Generator<String> {
+    private RangeOption<Double> range;
+    private int decimals;
+    private DecimalGenerator decimal;
+
+    public LatitudeGenerator(RangeOption range) {
+        this.decimals = 5;
+        this.decimal = new DecimalGenerator();
+        this.decimal.range(RangeOption.from(-90.0, 90.0));
+    }
+
+    /**
+     * Set the number of decimals to return
+     * @param noOfDecimals The number of decimals to return
+     * @return The same generator
+     */
+    public LatitudeGenerator decimals(int noOfDecimals) {
+        this.decimal.digits(noOfDecimals);
+        return this;
+    }
+
+    /**
+     * Set the minimum, and maximum latitude
+     * @param min Minimum latitude (inclusive)
+     * @param max Maximum latitude (inclusive)
+     * @return The same generator
+     */
+    public LatitudeGenerator range(double min, double max) {
+        this.decimal.range(min, max);
+        return this;
+    }
+
+    @Override
+    public String gen() {
+        return decimal.gen();
+    }
+}
