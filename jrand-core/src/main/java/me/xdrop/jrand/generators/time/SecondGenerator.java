396f25851f9ebe9051ebcdeb4aa2f8d0bc173e48
xdrop <xdrop.me@gmail.com>
2018-02-13 19:18:05 +0000

    Add draft minute generator

--- jrand-core/src/main/java/me/xdrop/jrand/generators/time/SecondGenerator.java
+++ jrand-core/src/main/java/me/xdrop/jrand/generators/time/SecondGenerator.java
@@ -2,10 +2,22 @@ package me.xdrop.jrand.generators.time;
 
 import me.xdrop.jrand.Generator;
 import me.xdrop.jrand.annotation.Facade;
-import me.xdrop.jrand.generators.basics.DecimalGenerator;
+import me.xdrop.jrand.generators.basics.NaturalGenerator;
 
 @Facade(accessor = "second")
-public abstract class SecondGenerator extends Generator<Integer> {
+public class SecondGenerator extends Generator<Integer> {
+
+    private NaturalGenerator nat;
+
+    public SecondGenerator() {
+        this.nat = new NaturalGenerator();
+        this.nat.min(0).max(59);
+    }
+
+    @Override
+    public Integer gen() {
+        return this.nat.gen();
+    }
 
 
 }
