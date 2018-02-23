49ebe260a780217f022d509b9a46147f759c6734
xdrop <xdrop.me@gmail.com>
2018-02-23 18:39:14 +0000

    Collect classes in map and update in order

--- jrand-annotation-processor/src/main/java/me/xdrop/jrand/annotation/processing/ProcessorRepository.java
+++ jrand-annotation-processor/src/main/java/me/xdrop/jrand/annotation/processing/ProcessorRepository.java
@@ -25,19 +25,16 @@ import java.util.Optional;
 public class ProcessorRepository {
     public static String importClassName = "javax.annotation.Generated";
     private Map<String, Integer> rounds;
-    private Map<String, CompilationUnit> compilationUnits;
+    private static Map<String, CompilationUnit> compilationUnits = new HashMap<>();
     private SourceRoot sourceRoot;
     private int noProcessors;
     private ImportDeclaration generatedImportDecl;
 
     public ProcessorRepository() {
         this.rounds = new HashMap<>();
-        this.compilationUnits = new HashMap<>();
         Path root = Paths.get("jrand-core", "src", "main", "java");
         sourceRoot = new SourceRoot(root);
         noProcessors = 2;
-        generatedImportDecl = LexicalPreservingPrinter.setup(JavaParser.parseImport("import " + importClassName + ";\n"));
-        generatedImportDecl.getData(LexicalPreservingPrinter.NODE_TEXT_DATA).addToken(0, 3, "\n");
     }
 
     private CompilationUnit parseCU(String pkg, String filename) {
@@ -61,12 +58,17 @@ public class ProcessorRepository {
     public CompilationUnit getCU(String pkg, String className) {
         CompilationUnit compilationUnit = compilationUnits.get(className);
         if (compilationUnit == null) {
+            System.out.println("Parsing " + className);
             compilationUnit = parseCU(pkg, className + ".java");
+            compilationUnits.put(className, compilationUnit);
+        } else {
+            System.out.println("Reusing " + className);
+            System.out.println(compilationUnit);
         }
         return compilationUnit;
     }
 
-    private TypeSpec generateClassWithMethods(String className, MethodSpec... methodSpecs) {
+    private TypeSpec generateClassWithMethods(String className, List<MethodSpec> methodSpecs) {
         TypeSpec.Builder classBuilder = TypeSpec.classBuilder(className);
         for (MethodSpec m : methodSpecs) {
             classBuilder.addMethod(m);
@@ -91,8 +93,13 @@ public class ProcessorRepository {
         return false;
     }
 
+    private ImportDeclaration newImport() {
+        ImportDeclaration decl = LexicalPreservingPrinter.setup(JavaParser.parseImport("import " + importClassName + ";\n"));
+        decl.getData(LexicalPreservingPrinter.NODE_TEXT_DATA).addToken(0, 3, "\n");
+        return decl;
+    }
 
-    public CompilationUnit addMethods(CompilationUnit source, String className, String pkg, MethodSpec... methodSpecs) {
+    public CompilationUnit addMethods(CompilationUnit source, String className, String pkg, List<MethodSpec> methodSpecs) {
         Optional<ClassOrInterfaceDeclaration> clazzInSource = getClassByName(source, className);
         TypeSpec builtClass = generateClassWithMethods(className, methodSpecs);
         String generatedToParse = JavaFile.builder(pkg, builtClass).indent("    ").build().toString();
@@ -101,7 +108,7 @@ public class ProcessorRepository {
         List<ConstructorDeclaration> constructorsToMerge = cuToMerge.findAll(ConstructorDeclaration.class);
 
         if (!hasGeneratedImport(source)) {
-            source.addImport(importClassName);
+            source.addImport(newImport());
         }
 
         if (!clazzInSource.isPresent()) {
@@ -121,10 +128,9 @@ public class ProcessorRepository {
     }
 
     public void writeTo(Path path, String className, CompilationUnit cu) throws IOException {
+        System.out.println("Writing ..." + className);
         try (FileWriter fw = new FileWriter(new File(path.toString(), className + ".java"))) {
-            if (cu != null) {
-                LexicalPreservingPrinter.print(cu, fw);
-            }
+            LexicalPreservingPrinter.print(cu, fw);
         }
     }
 
