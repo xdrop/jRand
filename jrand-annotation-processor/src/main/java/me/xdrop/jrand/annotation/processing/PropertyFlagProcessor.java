f169b4c8b212d23f97f5bd6092f38e518c657cf7
xdrop <xdrop.me@gmail.com>
2018-02-24 18:10:22 +0000

    Change the way Property processor works

--- jrand-annotation-processor/src/main/java/me/xdrop/jrand/annotation/processing/PropertyFlagProcessor.java
+++ jrand-annotation-processor/src/main/java/me/xdrop/jrand/annotation/processing/PropertyFlagProcessor.java
@@ -1,6 +1,8 @@
 package me.xdrop.jrand.annotation.processing;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.squareup.javapoet.MethodSpec;
 import me.xdrop.jrand.annotation.PropertyFlag;
 
 import javax.annotation.processing.ProcessingEnvironment;
@@ -13,12 +15,13 @@ import javax.tools.Diagnostic;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Set;
+import java.util.*;
 
 @SupportedAnnotationTypes("me.xdrop.jrand.annotation.PropertyFlag")
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 public class PropertyFlagProcessor extends BaseProcessor {
     private PropertyMethodGenerator propertyMethodGenerator;
+    private Map<ClassIdentifier, List<MethodSpec>> methodPerClass = new HashMap<>();
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
@@ -28,6 +31,11 @@ public class PropertyFlagProcessor extends BaseProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+        if (roundEnv.processingOver()){
+            return true;
+        }
+
         for (Element element : roundEnv.getElementsAnnotatedWith(PropertyFlag.class)) {
             if (element.getKind() != ElementKind.FIELD) {
                 getMessager().printMessage(Diagnostic.Kind.ERROR, "Annotation [PropertyFlag] should only be used on fields.");
@@ -35,22 +43,42 @@ public class PropertyFlagProcessor extends BaseProcessor {
             }
             VariableElement symbol = (VariableElement) element;
             PropertyFlag annotation = symbol.getAnnotation(PropertyFlag.class);
+
+            System.out.println(annotation.toString());
             PackageElement pkg = processingEnv.getElementUtils().getPackageOf(symbol);
             String className = symbol.getEnclosingElement().getSimpleName().toString();
-            CompilationUnit compilationUnit = getRepository().getCU(pkg.toString(), className);
+            CompilationUnit cu = getRepository().getCU(pkg.toString(), className);
+
+            ClassIdentifier id = new ClassIdentifier(className, pkg);
+            List<MethodSpec> methodsInClass = methodPerClass.computeIfAbsent(id, k -> new ArrayList<>());
+
+            methodsInClass.addAll(propertyMethodGenerator.buildMethods(cu, symbol,
+                    annotation.value()));
+        }
+
+
+        methodPerClass.forEach((k,v) -> {
+            PackageElement pkg = k.getPackageName();
             String fullPackage = pkg.getQualifiedName().toString();
-            CompilationUnit newCU = propertyMethodGenerator.buildMethod(compilationUnit, symbol,
-                    className, fullPackage,annotation.value());
             // Get the last part of the package eg. me.xdrop.generators.text would be text
             String lastPackage = getLastPackageName(pkg);
             Path path = Paths.get(getOutputPathGenerators().toString(), lastPackage);
+            String className = k.getClassName();
+            CompilationUnit cu = getRepository().getCU(pkg.toString(), className);
+            List<MethodSpec> methodsToAdd = v;
+            System.out.println("Writing..."  + className);
 
+            getRepository().addMethods(cu, className, fullPackage, methodsToAdd);
             try {
-                getRepository().writeTo(path, className, newCU);
+                getRepository().writeTo(path, className, cu);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        });
+
+
+
+
         return false;
     }
 }
